11847a51c3410934724078be0bfc3869445a1f99
eugene-ievlev <eugene.ievlev@gmail.com>
2024-11-22 14:16:10 +1100

    support IPv6

--- handler.js
+++ handler.js
@@ -10,20 +10,20 @@ module.exports.sync = async event => {
   // Create or grab the security group
   let sg = await aws.upsertSg(vpcId);
 
-  // Get list of current fastly ips
+  // Get list of current fastly IPs
   let ips = await fastly.getFastlyIps();
 
-  // Flatten current list of ips
+  // Flatten current list of IPs
   let currentIps = aws.getIpsFromSg(sg);
 
   // Get list of rules to add
   let newIps = helpers.toAdd(currentIps, ips);
-  console.log(`New IPs being added: ${newIps}`);
+  console.log(`New IPs being added: ${JSON.stringify(newIps)}`);
   await aws.addSgRules(sg, newIps);
 
   // Get list of rules to delete
   let oldIps = helpers.toRemove(currentIps, ips);
-  console.log(`Old IPs being removed: ${oldIps}`);
+  console.log(`Old IPs being removed: ${JSON.stringify(oldIps)}`);
   await aws.removeSgRules(sg, oldIps);
 
   return {
